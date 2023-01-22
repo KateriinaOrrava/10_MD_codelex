82ce8a080235cbc371f8ec500df5f6bd4c0bbc3a
Katerina Orrava <98848233+KateriinaOrrava@users.noreply.github.com>
2023-01-22 15:17:46 +0200

    added add option and css

--- src/scripts.ts
+++ src/scripts.ts
@@ -13,34 +13,40 @@ function generateCards(res: TODO[]) {
     const card = document.createElement('article');
     card.classList.add('taskCard__Wrapper');
     card.innerHTML = `
-            <div class="characterCard__ContentWrapper card-wrapper">
-            <div class="section image-section">
-                <figure class="image is-128x128">
-                    <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-            </div>
-            <div class="section heading-section">
-              <h2>${task.name}</h2>
-            </div>
-            <div class="section description-section">
-              <p>Description: ${task.description}</p>
-            </div>
-            <div class="section btn-section">
-                <div class="btn-section__edit action-btn">
-                    <button class="edit-task">Edit task</button>
-                    </div>
-                <div class="btn-section__delete action-btn">
-                    <button class="delete-task" id= "${task.id}">Delete task</button>
+            <div class="card-wrapper">
+                <div class="section image-section">
+                    <figure class="image card__image">
+                        <img  src="/assets/images/unsplash-o.jpg">
+                    </figure>
                 </div>
-            </div>
-            <div class="section form-section">
-          </div>
+                <div class="task-content">
+                    <div class="section heading-section">
+                        <h2>${task.name}</h2>
+                    </div>
+                    <div class="section description-section">
+                        <p>Description: ${task.description}</p>
+                    </div>
+                    <div class="section btn-section">
+                        <div class="btn-section__edit action-btn">
+                            <button class="edit-task" id= "edit-${task.id}">Edit task</button>
+                        </div>
+                        <div class="btn-section__delete action-btn">
+                            <button class="delete-task" id= "${task.id}">Delete task</button>
+                        </div> 
+                    </div>
+                    <div class="section edit-form-section-${task.id} d-none">
+                            <input class="input" id="edit-task-name-${task.id}" type="text" placeholder="Edit task name" required>
+                            <div id="msg3"></div>
+                            <textarea class="textarea" id="edit-task-description-${task.id}" placeholder="Edit task description" required></textarea>
+                            <div id="msg4"></div>
+                            <input class="button edit-new-task" id="edit-task-submit-${task.id}" type="submit" value="Update task">
+                            </div>
+                    </div>
+                </div>
           </article>`;
     ///
     cardContainer.appendChild(card);
   });
-
-  
 
   res.forEach((task) => {
     const deleteTaskButton = document.getElementById(`${task.id}`);
@@ -51,15 +57,47 @@ function generateCards(res: TODO[]) {
     });
   });
 
+  res.forEach((task) => {
+    const editTaskButton = document.getElementById(`edit-${task.id}`);
+    const editTaskSubmitButton = document.getElementById(`edit-task-submit-${task.id}`);
+    const editTaskName = document.querySelector<HTMLFormElement | null>(`#edit-task-name-${task.id}`);
+    const editTaskDescription = document.querySelector<HTMLFormElement | null>(`#edit-task-description-${task.id}`);
+    const editForm = document.querySelector<HTMLDivElement | null>(`.edit-form-section-${task.id}`);
+    const msg3 = document.getElementById('msg3');
+    const msg4 = document.getElementById('msg4');
+    const showAndHide = () => {
+      if (editForm.classList.contains('d-none')) {
+        editForm.classList.remove('d-none');
+      } else {
+        editForm.classList.add('d-none');
+      }
+    };
+    editTaskButton.addEventListener('click', () => {
+      showAndHide();
+    });
+    editTaskSubmitButton.addEventListener('click', () => {
+      console.log(editTaskName.value);
+      console.log(editTaskDescription.value);
+      if (editTaskName.value === '') {
+        msg3.innerHTML = 'Task cannot be blank';
+      } else if (editTaskDescription.value === '') {
+        msg4.innerHTML = 'Task cannot be blank';
+      } else {
+        axios.put<TODO>(`http://localhost:3004/Funny_TO_DO/${task.id}`, {
+          name: editTaskName.value,
+          description: editTaskDescription.value,
+        });
+        window.location.reload();
+      }
+    });
+  });
+// console.log(editTaskName);
 //   const editTaskButtons = document.querySelectorAll<HTMLButtonElement | null>('.edit-task');
 //   const TaskHeaders = document.querySelectorAll<HTMLHeadingElement | null>('h2');
 //   res.forEach((task) => {
 //     editTaskButtons[task.id].addEventListener('click', () => {
 //       TaskHeaders[task.id].innerHTML = 'book';
-//       axios.put<TODO>(`http://localhost:3004/Funny_TO_DO/${task.id}`, {
-//         name: 'Elephant',
-//         description: 'Hello',
-//       });
+//
 //       window.location.reload();
 //     });
 //   });
@@ -95,15 +133,3 @@ addNewTaskButton.addEventListener('click', () => {
     window.location.reload();
   }
 });
-// const addAnimalsWrapper = document.querySelector('.js-add-to-do-task');
-// const addAnimalsButton = addAnimalsWrapper.querySelector('button');
-// const addAnimalsPre = addAnimalsWrapper.querySelector('pre');
-
-// addAnimalsButton.addEventListener('click', () => {
-//   axios.post<TODO>('http://localhost:3004/Funny_TO_DO', {
-//     name: 'Elephantos',
-//     description: 'kiss an elephant',
-//   }).then(({ data }) => {
-//     addAnimalsPre.innerHTML = JSON.stringify(data);
-//   });
-// });
