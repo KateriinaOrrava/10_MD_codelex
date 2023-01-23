f624b766032e3b4c4cbc2a03e3a79e1b65a92e4b
Katerina Orrava <98848233+KateriinaOrrava@users.noreply.github.com>
2023-01-23 19:11:50 +0200

    edit the db file

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-console.log('Ready for coding');
 type TODO = {
   id: number;
   name: string;
   description: string;
 }
+
+const showAndHide2 = (el:HTMLDivElement) => {
+  if (el.classList.contains('d-none')) {
+    el.classList.remove('d-none');
+  } else {
+    el.classList.add('d-none');
+  }
+};
 
 function generateCards(res: TODO[]) {
   const cardContainer = document.querySelector<HTMLDivElement | null>('#card-container');
@@ -16,7 +23,7 @@ function generateCards(res: TODO[]) {
             <div class="card-wrapper">
                 <div class="section image-section">
                     <figure class="image card__image">
-                        <img  src="/assets/images/unsplash-o.jpg">
+                        <img  src="https://picsum.photos/id/${task.id + 10}/200/200?grayscale">
                     </figure>
                 </div>
                 <div class="task-content">
@@ -35,9 +42,9 @@ function generateCards(res: TODO[]) {
                         </div> 
                     </div>
                     <div class="section edit-form-section-${task.id} d-none">
-                            <input class="input" id="edit-task-name-${task.id}" type="text" placeholder="Edit task name" required>
+                            <input class="input" id="edit-task-name-${task.id}" type="text" placeholder="${task.name}" required>
                             <div class="validation-error" id="msg3"></div>
-                            <textarea class="textarea" id="edit-task-description-${task.id}" placeholder="Edit task description" required></textarea>
+                            <textarea class="textarea" id="edit-task-description-${task.id}" placeholder="${task.description}" required></textarea>
                             <div class="validation-error" id="msg4"></div>
                             <input class="button edit-new-task" id="edit-task-submit-${task.id}" type="submit" value="Update task">
                             </div>
@@ -48,16 +55,15 @@ function generateCards(res: TODO[]) {
     cardContainer.appendChild(card);
   });
 
+  // delete task
+  // edit task
   res.forEach((task) => {
     const deleteTaskButton = document.getElementById(`${task.id}`);
     deleteTaskButton.addEventListener('click', () => {
-      console.log(task.id);
       axios.delete<TODO>(`http://localhost:3004/Funny_TO_DO/${task.id}`);
       window.location.reload();
     });
-  });
 
-  res.forEach((task) => {
     const editTaskButton = document.getElementById(`edit-${task.id}`);
     const editTaskSubmitButton = document.getElementById(`edit-task-submit-${task.id}`);
     const editTaskName = document.querySelector<HTMLFormElement | null>(`#edit-task-name-${task.id}`);
@@ -65,19 +71,12 @@ function generateCards(res: TODO[]) {
     const editForm = document.querySelector<HTMLDivElement | null>(`.edit-form-section-${task.id}`);
     const msg3 = document.getElementById('msg3');
     const msg4 = document.getElementById('msg4');
-    const showAndHide = () => {
-      if (editForm.classList.contains('d-none')) {
-        editForm.classList.remove('d-none');
-      } else {
-        editForm.classList.add('d-none');
-      }
-    };
+
     editTaskButton.addEventListener('click', () => {
-      showAndHide();
+      showAndHide2(editForm);
     });
     editTaskSubmitButton.addEventListener('click', () => {
-      console.log(editTaskName.value);
-      console.log(editTaskDescription.value);
+      // formValidation
       if (editTaskName.value === '') {
         msg3.innerHTML = 'Task cannot be blank';
       } else if (editTaskDescription.value === '') {
@@ -93,11 +92,18 @@ function generateCards(res: TODO[]) {
   });
 }
 
-// šī koda daļa saņem DATA no API un tad nosūta to funkcijai, kas izveido kartiņas
+// create task
 axios.get<TODO[]>('http://localhost:3004/Funny_TO_DO')
   .then(({ data }) => {
     generateCards(data);
   });
+
+// add new task
+const addNewShowButton = document.querySelector<HTMLButtonElement | null>('.action-btn__add-new');
+const formContainer = document.querySelector<HTMLDivElement | null>('.form-container');
+addNewShowButton.addEventListener('click', () => {
+  showAndHide2(formContainer);
+});
 
 const addNewTaskHeading = document.querySelector<HTMLFormElement | null>('.input');
 const addNewTaskDescription = document.querySelector<HTMLFormElement | null>('.textarea');
@@ -106,13 +112,12 @@ const msg1 = document.getElementById('msg1');
 const msg2 = document.getElementById('msg2');
 
 addNewTaskButton.addEventListener('click', () => {
-  // formValidation();
+  // formValidation
   if (addNewTaskHeading.value === '') {
     msg1.innerHTML = 'Task cannot be blank';
   } else if (addNewTaskDescription.value === '') {
     msg2.innerHTML = 'Task cannot be blank';
   } else {
-    console.log('success');
     msg1.innerHTML = '';
     msg2.innerHTML = '';
     axios.post<TODO>('http://localhost:3004/Funny_TO_DO', {
